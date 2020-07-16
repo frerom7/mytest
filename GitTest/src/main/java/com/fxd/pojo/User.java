b6a6e889ce95a50028437162abe1549f62e473f9
Dave <747516124@qq.com>
2020-07-16 20:12:34 +0800

    firstCommit

---
+++ GitTest/src/main/java/com/fxd/pojo/User.java
@@ -0,0 +1,26 @@
+package com.fxd.pojo;
+
+/**
+ * @author 冯晓东
+ * @date 2020/7/16  20:07
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
